f35714850bb8d3e9fa6c1355246375ca28a8a609
yanxiaolu <yanxiaolu@gmail.com>
2022-06-02 11:40:07 +0800

    数据库上下文继承了Identity的表结构

--- AmazBlog.Web/Data/ApplicationDbContext.cs
+++ AmazBlog.Web/Data/ApplicationDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using AmazBlog.Infrastructure;
+using AmazBlog.Share;
 
 namespace AmazBlog.Web.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Posts> Posts { get; set; }
+    public DbSet<Options> Options { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
