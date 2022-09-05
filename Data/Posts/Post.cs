61140d77ef473381d4e7b50e1ca7daf8bec4b5a5
yanxiaolu <yanxiaolu@gmail.com>
2022-09-05 22:20:27 +0800

    add comment class

--- Data/Posts/Post.cs
+++ Data/Posts/Post.cs
@@ -6,6 +6,8 @@ public class Posts
     public int Id { get; set; }
     public string Title { get; set; }
     public string Content { get; set; }
-
+    public DateTime PublishTime { get; set; }
+    public DateTime UpdateTime { get; set; }
+    public List<Comments> Comments { get; set; }
 
 }
