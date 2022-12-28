de7466d7fbe6073df28554599274e58075951422
yanxiaolu <yanxiaolu@gmail.com>
2022-12-28 21:51:24 +0800

    Reset Project

---
+++ AmazBlog.Theme1/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "AmazBlog.Theme1",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "AmazBlog.Theme1"
+)]
