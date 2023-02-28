1355dd82bfe0179cc6319a0eb9295849358f9157
yanxiaolu <yanxiaolu@users.noreply.github.com>
2023-02-28 09:25:32 +0000

    Initial Orchard Project

---
+++ Orchard.GetGPT.Theme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Orchard.GetGPT.Theme",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "Orchard.GetGPT.Theme"
+)]
