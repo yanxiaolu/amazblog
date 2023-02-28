c8c2418ee1de0e66d1adc36e9946a1fa355f43d3
yanxiaolu <yanxiaolu@users.noreply.github.com>
2023-02-28 09:15:13 +0000

    delete old file

--- AmazBlog.Theme1/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "AmazBlog.Theme1",
-    Author = "Yanxl",
-    Website = "https://orchardcore.net",
-    Version = "0.0.1",
-    Description = "AmazBlog.Theme1"
-)]
