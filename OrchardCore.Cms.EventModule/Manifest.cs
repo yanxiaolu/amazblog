6430569dd70eef2aa6a6d6986f1e42b0f3afe78a
Yan Xiaolu <yanxiaolu@shanlitech.com>
2025-04-10 15:39:38 +0800

    添加 OrchardCore.Cms.EventModule 模块，包括 HomeController、Manifest、Startup 和视图文件

---
+++ OrchardCore.Cms.EventModule/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "OrchardCore.Cms.EventModule",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "OrchardCore.Cms.EventModule",
+    Category = "Content Management"
+)]
