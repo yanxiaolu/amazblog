612ac439116802f821cbba966a7686bb587d69cd
Yan Xiaolu <yanxiaolu@shanlitech.com>
2025-04-15 18:37:23 +0800

    初始化模板代码

---
+++ OrchardCore.Cms.EventModule/Views/Event/backupIndex.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrchardCore.Cms.EventModule.Views.Event
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
